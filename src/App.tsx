4658e762c03ab08a63f7a0919e70de591e94636c
muhammadiso0203 <muhammmadiso0203@gmail.com>
2025-11-26 20:23:24 +0500

    Bot

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { memo } from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='text-[10px]'>Assalomu alaykum bot app botimizga hush kelibsiz</h2>
+      <p>Sizni botimizda ko`rganimizdan hursandmiz</p>
+    </div>
+  );
+};
+
+export default memo(App);
