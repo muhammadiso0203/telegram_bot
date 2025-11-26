67294f0f1920092feec26599f70a92e3d3de0dd1
muhammadiso0203 <muhammmadiso0203@gmail.com>
2025-11-26 20:29:17 +0500

    Bot

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 
 const App = () => {
   return (
-    <div>
+    <div className='bg-white'>
       <h2 className='text-[10px]'>Assalomu alaykum bot app botimizga hush kelibsiz</h2>
       <p>Sizni botimizda ko`rganimizdan hursandmiz</p>
     </div>
